d7a0bf5ec654c7f236ff0a5debefa792c4282d88
Gabriel.Luiz <62039117+GabrielLuizLupionVinhaes@users.noreply.github.com>
2021-06-01 18:03:01 -0300

    Add files via upload

---
+++ Troco/SistemaDeTroco.Designer.cs
@@ -0,0 +1,387 @@
+﻿namespace Troco
+{
+    partial class FrmST
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPreco = new System.Windows.Forms.Label();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.btnCalcula = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txt100 = new System.Windows.Forms.TextBox();
+            this.txt50 = new System.Windows.Forms.TextBox();
+            this.txt20 = new System.Windows.Forms.TextBox();
+            this.txt10 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt50C = new System.Windows.Forms.TextBox();
+            this.txt25C = new System.Windows.Forms.TextBox();
+            this.txt10C = new System.Windows.Forms.TextBox();
+            this.txt5C = new System.Windows.Forms.TextBox();
+            this.txt1C = new System.Windows.Forms.TextBox();
+            this.txtLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPreco
+            // 
+            this.lblPreco.AutoSize = true;
+            this.lblPreco.Location = new System.Drawing.Point(39, 122);
+            this.lblPreco.Name = "lblPreco";
+            this.lblPreco.Size = new System.Drawing.Size(35, 13);
+            this.lblPreco.TabIndex = 0;
+            this.lblPreco.Text = "Preço";
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(80, 119);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(100, 20);
+            this.txtPreco.TabIndex = 1;
+            // 
+            // btnCalcula
+            // 
+            this.btnCalcula.Location = new System.Drawing.Point(80, 154);
+            this.btnCalcula.Name = "btnCalcula";
+            this.btnCalcula.Size = new System.Drawing.Size(100, 43);
+            this.btnCalcula.TabIndex = 4;
+            this.btnCalcula.Text = "Calcular troco";
+            this.btnCalcula.UseVisualStyleBackColor = true;
+            this.btnCalcula.Click += new System.EventHandler(this.btnCalcula_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "R$ 100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(330, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "R$ 50";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(403, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "R$ 20";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(245, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "R$ 10";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(330, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "R$ 5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(409, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "R$ 2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(245, 164);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "R$ 1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(330, 164);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "R$ 0,50";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(409, 164);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "R$ 0,25";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(242, 196);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(45, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "R$ 0,10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(330, 196);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(45, 13);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "R$ 0,05";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(409, 196);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(45, 13);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "R$ 0,01";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(265, 47);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(215, 20);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "Troco em notas e/ou moedas";
+            // 
+            // txt100
+            // 
+            this.txt100.BackColor = System.Drawing.SystemColors.Window;
+            this.txt100.Enabled = false;
+            this.txt100.Location = new System.Drawing.Point(293, 96);
+            this.txt100.Name = "txt100";
+            this.txt100.Size = new System.Drawing.Size(31, 20);
+            this.txt100.TabIndex = 18;
+            // 
+            // txt50
+            // 
+            this.txt50.Enabled = false;
+            this.txt50.Location = new System.Drawing.Point(372, 96);
+            this.txt50.Name = "txt50";
+            this.txt50.Size = new System.Drawing.Size(31, 20);
+            this.txt50.TabIndex = 19;
+            // 
+            // txt20
+            // 
+            this.txt20.Enabled = false;
+            this.txt20.Location = new System.Drawing.Point(460, 96);
+            this.txt20.Name = "txt20";
+            this.txt20.Size = new System.Drawing.Size(31, 20);
+            this.txt20.TabIndex = 20;
+            // 
+            // txt10
+            // 
+            this.txt10.Enabled = false;
+            this.txt10.Location = new System.Drawing.Point(293, 128);
+            this.txt10.Name = "txt10";
+            this.txt10.Size = new System.Drawing.Size(31, 20);
+            this.txt10.TabIndex = 21;
+            // 
+            // txt5
+            // 
+            this.txt5.Enabled = false;
+            this.txt5.Location = new System.Drawing.Point(372, 129);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(31, 20);
+            this.txt5.TabIndex = 22;
+            // 
+            // txt2
+            // 
+            this.txt2.Enabled = false;
+            this.txt2.Location = new System.Drawing.Point(460, 129);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(31, 20);
+            this.txt2.TabIndex = 23;
+            // 
+            // txt1
+            // 
+            this.txt1.Enabled = false;
+            this.txt1.Location = new System.Drawing.Point(293, 161);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(31, 20);
+            this.txt1.TabIndex = 24;
+            // 
+            // txt50C
+            // 
+            this.txt50C.Enabled = false;
+            this.txt50C.Location = new System.Drawing.Point(372, 161);
+            this.txt50C.Name = "txt50C";
+            this.txt50C.Size = new System.Drawing.Size(31, 20);
+            this.txt50C.TabIndex = 25;
+            // 
+            // txt25C
+            // 
+            this.txt25C.Enabled = false;
+            this.txt25C.Location = new System.Drawing.Point(460, 161);
+            this.txt25C.Name = "txt25C";
+            this.txt25C.Size = new System.Drawing.Size(31, 20);
+            this.txt25C.TabIndex = 26;
+            // 
+            // txt10C
+            // 
+            this.txt10C.Enabled = false;
+            this.txt10C.Location = new System.Drawing.Point(293, 193);
+            this.txt10C.Name = "txt10C";
+            this.txt10C.Size = new System.Drawing.Size(31, 20);
+            this.txt10C.TabIndex = 27;
+            // 
+            // txt5C
+            // 
+            this.txt5C.Enabled = false;
+            this.txt5C.Location = new System.Drawing.Point(372, 193);
+            this.txt5C.Name = "txt5C";
+            this.txt5C.Size = new System.Drawing.Size(31, 20);
+            this.txt5C.TabIndex = 28;
+            // 
+            // txt1C
+            // 
+            this.txt1C.Enabled = false;
+            this.txt1C.Location = new System.Drawing.Point(460, 193);
+            this.txt1C.Name = "txt1C";
+            this.txt1C.Size = new System.Drawing.Size(31, 20);
+            this.txt1C.TabIndex = 29;
+            // 
+            // txtLimpar
+            // 
+            this.txtLimpar.Location = new System.Drawing.Point(391, 230);
+            this.txtLimpar.Name = "txtLimpar";
+            this.txtLimpar.Size = new System.Drawing.Size(100, 43);
+            this.txtLimpar.TabIndex = 30;
+            this.txtLimpar.Text = "Limpar";
+            this.txtLimpar.UseVisualStyleBackColor = true;
+            this.txtLimpar.Click += new System.EventHandler(this.txtLimpar_Click);
+            // 
+            // FrmST
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 311);
+            this.Controls.Add(this.txtLimpar);
+            this.Controls.Add(this.txt1C);
+            this.Controls.Add(this.txt5C);
+            this.Controls.Add(this.txt10C);
+            this.Controls.Add(this.txt25C);
+            this.Controls.Add(this.txt50C);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt10);
+            this.Controls.Add(this.txt20);
+            this.Controls.Add(this.txt50);
+            this.Controls.Add(this.txt100);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcula);
+            this.Controls.Add(this.txtPreco);
+            this.Controls.Add(this.lblPreco);
+            this.Name = "FrmST";
+            this.Text = "Sistema de Troco";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPreco;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.Button btnCalcula;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txt100;
+        private System.Windows.Forms.TextBox txt50;
+        private System.Windows.Forms.TextBox txt20;
+        private System.Windows.Forms.TextBox txt10;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt50C;
+        private System.Windows.Forms.TextBox txt25C;
+        private System.Windows.Forms.TextBox txt10C;
+        private System.Windows.Forms.TextBox txt5C;
+        private System.Windows.Forms.TextBox txt1C;
+        private System.Windows.Forms.Button txtLimpar;
+    }
+}
+
---
+++ Troco/SistemaDeTroco.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Troco
+{
+    public partial class FrmST : Form
+    {
+        public FrmST()
+        {
+            InitializeComponent();
+        }
+        double preco = 0.0;
+        
+        private void btnCalcula_Click(object sender, EventArgs e)
+        {
+            preco = Convert.ToDouble(txtPreco.Text);
+
+            //Calcula a quantidade de notas de R$100
+            int quantia = 0; 
+            while(preco >= 100)
+            {                               
+                preco = preco - 100;
+                quantia = quantia + 1;                
+            }
+            txt100.Text = Convert.ToString(quantia);
+
+            //Calcula a quantidade de notas de R$50
+            quantia = 0;
+            while(preco >= 50)
+            {
+                preco = preco - 50;
+               quantia = quantia + 1;
+            }
+            txt50.Text = Convert.ToString(quantia);
+
+            //Calcula a quantidade de notas de R$20
+            quantia = 0;
+            while(preco >= 20)
+            {
+                preco = preco - 20;
+                quantia = quantia + 1;
+            }
+            txt20.Text = Convert.ToString(quantia);
+
+            //Calcula a quantidade de notas de R$10
+            quantia = 0;
+            while(preco >= 10)
+            {
+                preco = preco -  10;
+                quantia = quantia + 1;
+            }
+            txt10.Text = Convert.ToString(quantia);
+
+            //Calcula a quantidade de notas de R$5
+            quantia = 0;
+            while (preco >= 5)
+            {
+                preco = preco -  5;
+                quantia = quantia + 1;
+            }
+            txt5.Text = Convert.ToString(quantia);
+
+            //Calcula a quantidade de notas de R$2
+            quantia = 0;
+            while (preco >= 2)
+            {
+                preco = preco -  2;
+                quantia = quantia + 1;
+            }
+            txt2.Text = Convert.ToString(quantia);
+
+            //Calcula a quantidade de moedas de R$1
+            quantia = 0;
+            while (preco >= 1)
+            {
+                preco = preco -  1;
+                quantia = quantia + 1;
+            }
+            txt1.Text = Convert.ToString(quantia);
+
+            //Calcula a quantidade de moedas de R$0,50
+            quantia = 0;
+            while (preco >= 0.50)
+            {
+                preco = preco -  0.50;
+                quantia = quantia + 1;
+            }
+            txt50C.Text = Convert.ToString(quantia);
+
+            //Calcula a quantidade de moedas de R$0,25
+            quantia = 0;
+            while (preco >= 0.25)
+            {
+                preco = preco -  0.25;
+                quantia = quantia + 1;
+            }
+            txt25C.Text = Convert.ToString(quantia);
+
+            //Calcula a quantidade de moedas de R$0,10
+            quantia = 0;
+            while (preco >= 0.10)
+            {
+                preco = preco -  0.10;
+                quantia = quantia + 1;
+            }
+            txt10C.Text = Convert.ToString(quantia);
+
+            //Calcula a quantidade de moedas de R$0,05
+            quantia = 0;
+            while (preco >= 0.05)
+            {
+                preco = preco -  0.05;
+                quantia = quantia + 1;
+            }
+            txt5C.Text = Convert.ToString(quantia);
+
+            //Calcula a quantidade de moedas de R$0,01
+            quantia = 0;
+            while (preco >= 0.01)
+            {
+                preco = preco -  0.01;
+                quantia = quantia + 1;
+            }
+            txt1C.Text = Convert.ToString(quantia);
+
+
+        }
+
+        private void txtLimpar_Click(object sender, EventArgs e)
+        {
+            //Limpar TextBox
+            txtPreco.Text = "";
+            txt100.Text = "";
+            txt50.Text = "";
+            txt20.Text = "";
+            txt10.Text = "";
+            txt5.Text = "";
+            txt2.Text = "";
+            txt1.Text = "";
+            txt50C.Text = "";
+            txt25C.Text = "";
+            txt10C.Text = "";
+            txt5C.Text = "";
+            txt1C.Text = "";
+
+        }
+    }
+}
